eb2eed7b31b4b399b037a8ae92c45721e4754c91
prakharr5 <prakharkumar.srivastava.22cse@bmu.edu.in>
2024-09-09 19:32:34 +0530

    second commit

--- src/assets/assets.jsx
+++ src/assets/assets.jsx
@@ -1,7 +1,23 @@
 import picOne from "./images/picOne.jpg";
+import acm from "./images/acm.png";
+import sarc from "./images/sarc.png";
+import shivang from "./images/shivang.png";
+import Instagram from "./images/Instagram.png";
+import Internet from "./images/Internet.png";
+import Linkedin from "./images/LinkedIn.png";
+import Whtsp from "./images/WhatsApp.png";
+import over from "./images/over.png";
 
 const assets = {
   picOne,
+  acm,
+  over,
+  sarc,
+  shivang,
+  Whtsp,
+  Instagram,
+  Linkedin,
+  Internet,
 };
 
 export default assets;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,6 +8,16 @@ function Hero() {
       <div className="main">
         <h1>DADDY'S HOME</h1>
         <img srcSet={assets.picOne} alt="ACM" />
+        <img srcSet={assets.over} alt="ACM" />
+        <img srcSet={assets.shivang} alt="ACM" />
+        <div className="blackbg">
+          <img srcSet={assets.acm} alt="ACM" />
+          <img srcSet={assets.sarc} alt="ACM" />
+          <img srcSet={assets.Whtsp} alt="ACM" />
+          <img srcSet={assets.Internet} alt="ACM" />
+          <img srcSet={assets.Instagram} alt="ACM" />
+          <img srcSet={assets.Linkedin} alt="ACM" />
+        </div>
       </div>
     </>
   );
